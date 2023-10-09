f999c3e46344fa35cc913a3858abaf845cd8c40e
poojanenavath123 <147391570+poojanenavath123@users.noreply.github.com>
2023-10-09 15:51:34 +0530

    Create abc.py
    This is my first file

---
+++ abc.py
@@ -0,0 +1,8 @@
+# Input filename from the user
+filename = input("Input the Filename: ")
+
+# Get the file extension
+extension = filename.split('.')[-1]
+
+# Print the result
+print(f"The extension of the file is: '{extension}'")
