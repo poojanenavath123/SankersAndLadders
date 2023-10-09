27890c2c5996cfebefac3d20578ee902db6395af
poojanenavath123 <147391570+poojanenavath123@users.noreply.github.com>
2023-10-09 16:00:11 +0530

    circle.py
    This file print circle Radius on the screen

---
+++ circle.py
@@ -0,0 +1,10 @@
+import math
+
+# Input radius from the user
+radius = float(input("Input the radius of the circle: "))
+
+# Calculate area
+area = math.pi * (radius ** 2)
+
+# Print the result
+print(f"The area of the circle with radius {radius} is: {area}")
